597b40b2326ae70fd826ec76655a139ced19c8f2
Nicholas Mayne <Jetski5822@gmail.com>
2012-06-16 12:31:51 +0100

    added migration class to make sure Categories and Tags are attached to blogpost

---
+++ Migrations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Contrib.ImportExport.Services;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Data.Migration;
+
+namespace Contrib.ImportExport {
+    public class Migrations : DataMigrationImpl {
+        private readonly ITaxonomyImportService _taxonomyImportService;
+
+        public Migrations(ITaxonomyImportService taxonomyImportService) {
+            _taxonomyImportService = taxonomyImportService;
+        }
+
+        public int Create() {
+            var categoryTaxonomyPart = _taxonomyImportService.CreateTaxonomy("Categories");
+            var tagsTaxonomyPart = _taxonomyImportService.CreateTaxonomy("Tags");
+
+            ContentDefinitionManager.AlterPartDefinition("BlogPostPart", builder => builder
+                .WithField("Categories", cfg => cfg
+                    .OfType("TaxonomyField")
+                    .WithSetting("TaxonomyFieldSettings.AllowCustomTerms", "true")
+                    .WithSetting("TaxonomyFieldSettings.Taxonomy", categoryTaxonomyPart.Name))
+                .WithField("Tags", cfg => cfg
+                    .OfType("TaxonomyField")
+                    .WithSetting("TaxonomyFieldSettings.AllowCustomTerms", "true")
+                    .WithSetting("TaxonomyFieldSettings.Taxonomy", tagsTaxonomyPart.Name)));
+
+            return 1;
+        }
+    }
+}
